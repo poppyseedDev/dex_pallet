25473f450a585fddce99ae0df43ff11d18fed4b7
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-09-03 11:03:04 +0200

    add 6551 pallet

---
+++ pallets/erc6551/src/lib.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{pallet_prelude::*, traits::fungibles};
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/reference/frame-pallets/>
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
